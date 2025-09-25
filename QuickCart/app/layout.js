239a398051221d9dfe64401176d2490a4323fa79
Ambarubale6061 <ambarubale3333@gmail.com>
2025-09-25 12:43:57 +0530

    updated

--- QuickCart/app/layout.js
+++ QuickCart/app/layout.js
@@ -4,22 +4,24 @@ import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const outfit = Outfit({ subsets: ["latin"], weight: ["300", "400", "500"] });
+const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
-  title: "QuickCart",
-  description: "Shop the best products online with QuickCart ",
+  title: "QuickCart - GreatStack",
+  description: "E-Commerce with Next.js ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${outfit.className} antialiased text-gray-700`}>
+        <body className={`${outfit.className} antialiased text-gray-700`} >
           <Toaster />
-          <AppContextProvider>{children}</AppContextProvider>
+          <AppContextProvider>
+            {children}
+          </AppContextProvider>
         </body>
       </html>
-    </ClerkProvider>
+      </ClerkProvider>
   );
 }
